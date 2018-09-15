09dbaf1f8e0b051d449e85b3b71ec9b9be758cda
FlaviusHouk <flaviusglamfenix@gmail.com>
2018-09-15 20:12:28 +0300

    Update Cluster Analisys Algorythm

--- TagManager/Converters/ColorPaleteCreator.cs
+++ TagManager/Converters/ColorPaleteCreator.cs
@@ -10,26 +10,42 @@ using System.Threading.Tasks;
 using System.Windows.Data;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using TagManager.Helpers;
 
 namespace TagManager.Converters
 {
-    class ColorCluster
+    class ColorCluster : ICluster<Color>
     {
-        public Color MainColor { get; private set; }
-        public int Close { get; set; } = 0;
+        public Color Main { get; private set; }
+        public int Count { get; set; } = 0;
 
         public ColorCluster(Color main)
         {
-            MainColor = main;
+            Main = main;
         }
         
         public double CheckDistance(Color toCheck)
         {
-            double redDiff = Math.Pow(MainColor.R - toCheck.R, 2);
-            double greenDiff = Math.Pow(MainColor.G - toCheck.G, 2);
-            double blueDiff = Math.Pow(MainColor.B - toCheck.B, 2);
+            double redDiff = Math.Pow(Main.R - toCheck.R, 2);
+            double greenDiff = Math.Pow(Main.G - toCheck.G, 2);
+            double blueDiff = Math.Pow(Main.B - toCheck.B, 2);
 
             return Math.Sqrt(redDiff + greenDiff + blueDiff);
+        }
+
+        public override int GetHashCode()
+        {
+            return Main.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            ColorCluster that = obj as ColorCluster;
+
+            if (that == null)
+                return false;
+
+            return that.Main.Equals(Main) && Count == that.Count;
         }
     }
 
@@ -60,7 +76,7 @@ namespace TagManager.Converters
                 }
                 catch (Exception e)
                 {
-                    return new SolidColorBrush(Color.FromRgb(255, 255, 255));
+                    return Brushes.White;
                 }
             }
             image.Freeze();
@@ -79,23 +95,43 @@ namespace TagManager.Converters
 
             List<Color> pixels = new List<Color>(data.Length / 3);
 
-            if (image.Format.BitsPerPixel == 24)
+            if (image.Format == PixelFormats.Bgr24)
             {
                 for (int i = 0; i < data.Length; i += 3)
                 {
                     pixels.Add(Color.FromRgb(data[i + 2], data[i + 1], data[i]));
                 }
-
-                Debug.WriteLine(DateTime.Now - start);
-                start = DateTime.Now;
             }
+            else if (image.Format == PixelFormats.Bgr32)
+            {
+                for (int i = 0; i < data.Length; i += 4)
+                {
+                    pixels.Add(Color.FromRgb(data[i + 2], data[i + 1], data[i]));
+                }
+            }
+            else if (image.Format == PixelFormats.Rgb24)
+            {
+                for (int i = 0; i < data.Length; i += 3)
+                {
+                    pixels.Add(Color.FromRgb(data[i], data[i + 1], data[i + 2]));
+                }
+            }
+            
 
             if (pixels.Count != 0)
             {
-                _pixelData = pixels;
-                _asyncEmul = new ManualResetEvent(false);
-                ClusterAnalysisAsync();
-                _asyncEmul.WaitOne();
+                Random rand = new Random((int)DateTime.Now.Ticks);
+                _clusterSet = new List<ColorCluster>(10);
+                for (int i = 0; i < 10; i++)
+                {
+                    _clusterSet.Add(new ColorCluster(pixels.ElementAt(rand.Next(pixels.Count()))));
+                }
+                ClusterAnalisis<Color, ColorCluster> helper = new ClusterAnalisis<Color, ColorCluster>(pixels, _clusterSet);
+                helper.RunAnalysisAsync(4);
+                IEnumerable<ColorCluster> main = _clusterSet.OrderBy(c => c.Count).Skip(8).ToArray();
+                helper = new ClusterAnalisis<Color, ColorCluster>(pixels, main);
+                helper.RunAnalysisAsync(4);
+                _clusterSet = main.ToList();
 
                 Debug.WriteLine(DateTime.Now - start);
                 start = DateTime.Now;
@@ -104,9 +140,11 @@ namespace TagManager.Converters
             }
             else
             {
-                return new SolidColorBrush(Color.FromRgb(255, 255, 255));
+                return Brushes.White;
             }
         }
+
+        private List<ColorCluster> _clusterSet;
 
         private Color ClusterAnalysis(IEnumerable<Color> pixels)
         {
@@ -121,91 +159,27 @@ namespace TagManager.Converters
             foreach (Color pixel in pixels)
             {
                 var distances = clusters.Select(cluster => cluster.CheckDistance(pixel)).ToList();
-                clusters.ElementAt(distances.IndexOf(distances.Min())).Close++;
+                clusters.ElementAt(distances.IndexOf(distances.Min())).Count++;
             }
 
-            var counts = clusters.Select(cluster => cluster.Close).ToList();
+            var counts = clusters.Select(cluster => cluster.Count).ToList();
 
-            return clusters.ElementAt(counts.IndexOf(counts.Max())).MainColor;
-        }
-
-        ManualResetEvent _asyncEmul;
-
-        private IEnumerable<Color> _pixelData;
-        private List<IEnumerable<ColorCluster>> _clusterSets = new List<IEnumerable<ColorCluster>>();
-        private int _sharedCouner = 0;
-        private object _indexLocker = new object();
-        private object _clusterLocker = new object();
-
-        private void ClusterAnalysisAsync()
-        {
-            Random rand = new Random((int)DateTime.Now.Ticks);
-
-            for (int j = 0; j < 1; j++)
-            {
-                List<ColorCluster> clusters = new List<ColorCluster>(10);
-                for (int i = 0; i < 10; i++)
-                {
-                    clusters.Add(new ColorCluster(_pixelData.ElementAt(rand.Next(_pixelData.Count()))));
-                }
-                _clusterSets.Add(clusters);
-            }
-
-            Task firstTask = Task.Run(new Action(FindColor));
-            Task secondTask = Task.Run(new Action(FindColor));
-            Task thirdTask = Task.Run(new Action(FindColor));
-            Task fourthTask = Task.Run(new Action(FindColor));
-        } 
-
-        private void FindColor()
-        {
-            int index;
-            lock (_indexLocker)
-            {
-                index = _sharedCouner;
-                _sharedCouner++;
-            }
-
-            while (index < _pixelData.Count())
-            {
-                Color pixel = _pixelData.ElementAt(index);
-                foreach (IEnumerable<ColorCluster> clusters in _clusterSets)
-                {
-                    var distances = clusters.Select(cluster => cluster.CheckDistance(pixel)).ToList();
-                    ColorCluster c = clusters.ElementAt(distances.IndexOf(distances.Min()));
-
-                    lock (_clusterLocker)
-                    {
-                        c.Close++;
-                    }
-                }
-
-                lock (_indexLocker)
-                {
-                    index = _sharedCouner;
-                    _sharedCouner++;
-                }
-            }
-
-            _asyncEmul.Set();
+            return clusters.ElementAt(counts.IndexOf(counts.Max())).Main;
         }
 
         private Color GetMainColor()
         {
             List<Color> rezults = new List<Color>();
-            foreach (IEnumerable<ColorCluster> clusters in _clusterSets)
-            {
-                var counts = clusters.Select(cluster => cluster.Close).ToList();
 
-                rezults.Add(clusters.ElementAt(counts.IndexOf(counts.Max())).MainColor);
-            }
+            var counts = _clusterSet.Select(cluster => cluster.Count).ToList();
+
+            rezults.Add(_clusterSet.ElementAt(counts.IndexOf(counts.Max())).Main);
 
             byte r = (byte)rezults.Select(c => (double)c.R).Average();
             byte g = (byte)rezults.Select(c => (double)c.G).Average();
             byte b = (byte)rezults.Select(c => (double)c.B).Average();
 
-            _sharedCouner = 0;
-            _clusterSets.Clear();
+            _clusterSet.Clear();
 
             return Color.FromRgb(r, g, b);
         }
---
+++ TagManager/Helpers/ClusterAnalisis.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TagManager.Helpers
+{
+    interface ICluster<T>
+    {
+        int Count { get; set; }
+        double CheckDistance(T obj);
+        T Main { get; }
+    }
+
+    class ClusterAnalisis<T, C> where C : ICluster<T>
+    {
+        private IEnumerable<C> _clusters;
+        private IEnumerable<T> _data;
+        private ManualResetEvent _ev;
+        private int _sharedCounter = 0;
+        private object _sharedLocker = new object();
+        private object _clusterLocker = new object();
+        List<Task> _tasks;
+
+        public ClusterAnalisis(IEnumerable<T> data, IEnumerable<C> clusters)
+        {
+            _data = data;
+            _clusters = clusters;
+            _ev = new ManualResetEvent(false);
+        }
+
+        public void RunAnalysis()
+        {
+            foreach (T item in _data)
+            {
+                var distances = _clusters.Select(cluster => cluster.CheckDistance(item)).ToList();
+                _clusters.ElementAt(distances.IndexOf(distances.Min())).Count++;
+            }
+        }
+
+        private void MultitaskAnalysis()
+        {
+            int index;
+            lock (_sharedLocker)
+            {
+                index = _sharedCounter;
+                _sharedCounter++;
+            }
+
+            while (index < _data.Count())
+            {
+                T pixel = _data.ElementAt(index);
+
+                var distances = _clusters.Select(cluster => cluster.CheckDistance(pixel)).ToList();
+                C c = _clusters.ElementAt(distances.IndexOf(distances.Min()));
+
+                lock (_clusterLocker)
+                {
+                    c.Count++;
+                }
+
+                lock (_sharedLocker)
+                {
+                    index = _sharedCounter;
+                    _sharedCounter++;
+                }
+            }
+
+        }
+
+        private int _done = 0;
+        private object _doneLock = new object();
+        public void AfterWork(Task task)
+        {
+            lock(_doneLock)
+                _done++;
+
+            if (_done == _tasks.Count)
+            {
+                _ev.Set();
+                _done = 0;
+            }
+        }
+
+        public void RunAnalysisAsync(int tasksCount)
+        {
+            _tasks = new List<Task>(tasksCount);
+            
+            for (int i = 0; i < tasksCount; i++)
+            {
+                _tasks.Add(Task.Run(new Action(MultitaskAnalysis)).ContinueWith(new Action<Task>(AfterWork)));
+            }
+
+            _ev.WaitOne();
+        }
+    }
+}
