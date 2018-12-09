58d3ce8f6e4e6c2a42400941df528d6c7b248435
FlaviusHouk <flaviusglamfenix@gmail.com>
2018-12-09 16:20:31 +0200

    End main integration test.

--- IntegrationTests/IntegrationTests.cs
+++ IntegrationTests/IntegrationTests.cs
@@ -32,6 +32,34 @@ namespace IntegrationTests
             Assert.AreEqual(tag.Accompaniment, libTag.FirstAlbumArtist);
             Assert.AreEqual(tag.Album, libTag.Album);
             Assert.AreEqual(tag.Image, (libTag.Pictures[0] as TagLib.Id3v2.AttachedPictureFrame).Data);
+            Assert.AreEqual(tag.Composer, libTag.FirstComposer ?? string.Empty);
+            Assert.AreEqual(tag.Conductor, libTag.Conductor ?? string.Empty);
+            Assert.AreEqual(tag.ContentGroup, libTag.Grouping ?? string.Empty);
+            Assert.AreEqual(tag.Copyright, libTag.Copyright ?? string.Empty);
+            //Assert.AreEqual(tag.EncodedBy, libTag.)
+            //Assert.AreEqual(tag.FileOwner, libTag.)
+            //Assert.AreEqual(tag.FileType, libTag.);
+            //Assert.AreEqual(tag.FullDate, libTag.);
+            Assert.AreEqual(tag.Genre.Split(new[] { ';', '/', ',' }), libTag.Genres);
+            //Assert.AreEqual(tag.HardwareSettings, libTag.);
+            //Assert.AreEqual(tag.ISRC, libTag.);
+            //Assert.AreEqual(tag.Key, libTag.);
+            //Assert.AreEqual(tag.Language, libTag.);
+            Assert.AreEqual(tag.Length, tag.Length);
+            Assert.AreEqual(tag.Lyrics, tag.Lyrics ?? string.Empty);
+            //Assert.AreEqual(tag.MediaSize, libTag.);
+            //Assert.AreEqual(tag.MediaType, libTag.T);
+            //Assert.AreEqual(tag.OriginalAuthor, libTag.);
+            //Assert.AreEqual(tag.OriginalFileName, libTag.);
+            //Assert.AreEqual(tag.PartOfASet, libTag.Disc); - different formats
+            //Assert.AreEqual(tag.PlaylistDelay, libTag.);
+            //Assert.AreEqual(tag.Publisher, libTag.Pu);
+            //Assert.AreEqual(tag.RadiostationName, libTag.);
+            //Assert.AreEqual(tag.RecodringDate, libTag.);
+            //Assert.AreEqual(tag.SubTitle, libTag.S);
+            Assert.AreEqual(string.IsNullOrEmpty(tag.Tempo) ? 0 : uint.Parse(tag.Tempo), libTag.BeatsPerMinute);
+            //Assert.AreEqual(string.IsNullOrEmpty(tag.TrackNumber) ? 0 : uint.Parse(tag.TrackNumber), libTag.Track); - different formats
+            Assert.AreEqual(string.IsNullOrEmpty(tag.Year) ? 0 : uint.Parse(tag.Year), libTag.Year);
         }
     }
 }
